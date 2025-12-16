ead98ca600d2282d4e55bc6378bbdccb066bc35b
Hagai Raja <hagairaja@mrscraper.com>
2025-12-16 23:38:13 +0000

    init

---
+++ credentials/MrscraperApi.credentials.ts
@@ -0,0 +1,42 @@
+import type {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class MrscraperApi implements ICredentialType {
+	name = 'mrscraperApi';
+
+	displayName = 'Mrscraper API';
+
+	// Link to your community node's README
+	documentationUrl = 'https://github.com/org/-mrscraper?tab=readme-ov-file#credentials';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'API Key',
+			name: 'apiKey',
+			type: 'string',
+			typeOptions: { password: true },
+			required: true,
+			default: '',
+		},
+	];
+
+	authenticate: IAuthenticateGeneric = {
+		type: 'generic',
+		properties: {
+			headers: {
+				'x-api-key': '={{$credentials.apiKey}}',
+			},
+		},
+	};
+
+	test: ICredentialTestRequest = {
+		request: {
+			baseURL: 'https://api.v3.app.mrscraper.com',
+			url: '/v1/user',
+		},
+	};
+}
---
+++ nodes/Mrscraper/Mrscraper.node.ts
@@ -0,0 +1,50 @@
+import { NodeConnectionTypes, type INodeType, type INodeTypeDescription } from 'n8n-workflow';
+import { userDescription } from './resources/user';
+import { companyDescription } from './resources/company';
+
+export class Mrscraper implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Mrscraper',
+		name: 'mrscraper',
+		icon: { light: 'file:mrscraper.svg', dark: 'file:mrscraper.dark.svg' },
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Interact with the Mrscraper API',
+		defaults: {
+			name: 'Mrscraper',
+		},
+		usableAsTool: true,
+		inputs: [NodeConnectionTypes.Main],
+		outputs: [NodeConnectionTypes.Main],
+		credentials: [{ name: 'mrscraperApi', required: true }],
+		requestDefaults: {
+			baseURL: 'https://api.v3.app.mrscraper.com',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'User',
+						value: 'user',
+					},
+					{
+						name: 'Company',
+						value: 'company',
+					},
+				],
+				default: 'user',
+			},
+			...userDescription,
+			...companyDescription,
+		],
+	};
+}
